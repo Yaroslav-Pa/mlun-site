1dfdad90694fe5c6fcbb27176addf4c9220e7464
Yaroslav-Pa <yaroslav.roro@gmail.com>
2024-01-09 17:47:57 +0200

    adding fort awesome

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from '../components/header';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
+import Layout from '../components/layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,8 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Header></Header>
-        {children}
+        <Layout>{children}</Layout>
       </body>
     </html>
   );
--- src/components/header.js
+++
@@ -1,6 +0,0 @@
-export default function header() {
-  return (
-    <div className="h-[100px] w-[100px] bg-red-700">
-    </div>
-  )
-}
